b7980d476722865b9467ebe1fadc76c2a81aaa07
svgistnlr <sv.gistnlr@gmail.com>
2016-01-24 09:52:19 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,126 @@
+<html>
+<head>
+<title>Ekart Shopping</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link href="style/style.css" rel="stylesheet" type="text/css" />
+<script type="javascript">
+function goToPage(var q = '')
+{
+    var initial = "http://www.flipkart.com/search?q=";
+    var extension = "html";
+
+    window.location(initial+q+extension);
+}
+</script>
+</head>
+<body>
+<center>
+  <div class="wrapper">
+    <div class="logo"> Gistkart Shopping</div>
+    <div class="menu">
+      <ul class="solidblockmenu">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="categories.html">Categories</a></li>
+        <li><a href="hotdeals.html">Hot deals</a></li>
+        <li><a href="contactus.html">Contact Us</a></li>
+        <li><a href="faq.html">FAQ's</a></li>
+      </ul>
+      <div class="clear"></div>
+    </div>
+  </div>
+ 
+<div class="search">
+    <div class="search-text"> Search the world of shopping!&nbsp;
+     &nbsp;&nbsp;</div>
+    <div style="float:left; margin-left:5px; margin-top:10px;">
+
+
+<form name="search" action="http://www.flipkart.com/search?q=(this.value)">
+<input type="text" name="q" value="" onChange="goToPage(this.value)"/>
+<input type="submit" value="submit"/>
+</form>
+
+</div>
+  </div>
+  <div class="hot-search">
+    <div class="hot-search-text"> Hot Searches: Apple iPod Nano, Hugo Boss Clothing, HD DVD Player, DSLR Camera, Toshiba 32&quot; LCD TV, Unsecured Loans, Excersise Machines..</div>
+  </div>
+  <div class="body-wrapper">
+    <div class="body-right">
+      <div class="body-right-box1">
+        <div class="right-topic-title"> Favourite Items</div>
+        <br />
+        <div style="width:170px;padding-left:10px;"> <img src="images/moto.jpg" style="float:left; margin-right: 0px;"/>
+          <div style="width:140px; text-align:right; padding-left:3px;">
+            <div class="product-title"> Moto G3</div>
+            <div class="product-price"> Just Rs.10,999/-</div>
+            <div class="product-compare"> <u>Compare..</u>&nbsp;<img src="images/proceed.jpg" style="padding-top: 3px;" alt="" /></div>
+          </div>
+        </div>
+        <br />
+        <div style="width:170px;padding-left:10px;padding-top:3px;"> <img src="images/lenovo.jpg" style="float:left; margin-right: 0px;" alt="" />
+          <div style="width:140px; text-align:right; padding-left:3px;">
+            <div class="product-title"> Lenovo Vibe P1M</div>
+            <div class="product-price"> Just Rs.7,499/-</div>
+            <div class="product-compare"> <u>Compare..</u>&nbsp;<img src="images/proceed.jpg" style="padding-top: 3px;" alt="" /></div>
+          </div>
+        </div>
+        <br />
+        <div style="width:170px;padding-left:10px;padding-top:1px;"> <img src="images/mobile.jpg" style="float:left; margin-right: 0px;" alt="" />
+          <div style="width:140px; text-align:right; padding-left:3px;">
+            <div class="product-title"> Acer Z530</div>
+            <div class="product-price"> Just Rs.6,499/-</div>
+            <div class="product-compare"> <u>Compare..</u>&nbsp;<img src="images/proceed.jpg" style="padding-top: 3px;" alt="" /></div>
+          </div>
+        </div>
+        <br />
+        <div style="width:170px;padding-left:10px;padding-top:2px;"> <img src="images/mobile.jpg" style="float:left; margin-right: 0px;" alt="" />
+          <div style="width:140px; text-align:right; padding-left:3px;">
+            <div class="product-title"> Apple iPhone 5s</div>
+            <div class="product-price"> Just Rs.23,499/-</div>
+            <div class="product-compare"> <u>Compare..</u>&nbsp;<img src="images/proceed.jpg" style="padding-top: 3px;" alt="" /></div>
+          </div>
+        </div>
+        <br />
+        <div style="width:170px;padding-left:10px;padding-top:2px;"> <img src="images/mobile.jpg" style="float:left; margin-right: 0px;" alt="" />
+          <div style="width:140px; text-align:right; padding-left:3px;">
+            <div class="product-title"> RedMi 2 Prime</div>
+            <div class="product-price"> Just Rs.6,999/-</div>
+            <div class="product-compare"> <u>Compare..</u>&nbsp;<img src="images/proceed.jpg" style="padding-top: 3px;" alt="" /></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="divider2"> &nbsp;</div>
+      
+      <div class="body-left">
+      <div class="left-main-title"> Shopping Categories</div>
+      <div class="left-topic-title"><a href="http://www.flipkart.com/mobiles?otracker=hp_nmenu_sub_electronics_0_Mobiles"> Mobiles & Tablets</a></div>
+      <div class="left-topic-desc"> Apple,Microsoft,Motorola & More</div>
+      <div class="left-topic-title" style="padding-top:25px;"><a href="http://www.flipkart.com/computers/laptops?sid=6bo,b5g&otracker=hp_nmenu_sub_electronics_0_Laptops"> Laptops</a></div>
+      <div class="left-topic-desc"> Dell,HP,Acer & More</div>
+      <div class="left-topic-title" style="padding-top:28px;"><a href="http://www.flipkart.com/home-kitchen/home-appliances?otracker=nmenu_sub_electronics_1_Home%20Appliances">Electronics</a> </div>
+      <div class="left-topic-desc"> Philips,Godrej,Pigeon & More</div>
+      <div class="left-topic-title" style="padding-top:30px;"><a href="http://www.flipkart.com/books?otracker=hp_nmenu_sub_books-media_0_Books"> Books & Magzines</a></div>
+      <div class="left-topic-desc"> Chetan Baghat,Abdul Kalam & More</div>
+      <div class="left-topic-title" style="padding-top:30px;"><a href="http://www.flipkart.com/music?otracker=nmenu_sub_books-media_0_Music"> Music & Video</a></div>
+      <div class="left-topic-desc"> Regional,Pop,Bollywood & More</div>
+      <div class="left-topic-title" style="padding-top:30px;"><a href="http://www.jabong.com/online-sale/"> Clothing</a></div>
+      <div class="left-topic-desc"> Men,Women,Kids & More</div>
+      <div class="left-topic-title" style="padding-top:30px;"> <a href="http://www.marksandspencer.com/c/home-and-furniture">Home & Furniture</a></div>
+      <div class="left-topic-desc"> Chairs,Beds,Sofas & Cots</div>
+    </div>
+     
+  </div>
+  <div class="separator-main"> &nbsp;</div>
+  <div class="footer">
+  <p>All Rights Reserved @ GIST CART 2016</p>
+  <p>Contact information: <a href="mailto:gist.edu.in">
+  gist.edu.in</a>.</p>
+</div>
+
+
+  </div>
+  </body>
+</html>
